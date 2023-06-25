5b49b395b6bf446efdfea7ff74665fd3f8ffdebe
Ba-lambert <50544115+mugwanezalambert@users.noreply.github.com>
2023-06-25 21:33:21 +0200

    app initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className='text-blue-700 txt-3xl bg-black'>Welcome to typescript</p>
+    </div>
+  )
+}
+
+export default App
